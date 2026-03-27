eb29a4dc4ef141999bc4a76a76b58b43b66a49a7
Chris John <130599404+ChrisJohnNOAA@users.noreply.github.com>
2026-03-26 21:21:12 -0400

    Add import for java.nio.file.Files

--- WEB-INF/classes/gov/noaa/pfel/coastwatch/util/SharedWatchService.java
+++ WEB-INF/classes/gov/noaa/pfel/coastwatch/util/SharedWatchService.java
@@ -6,6 +6,7 @@ import com.cohort.util.String2;
 import java.io.IOException;
 import java.nio.file.FileStore;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
